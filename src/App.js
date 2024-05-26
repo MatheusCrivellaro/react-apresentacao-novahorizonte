60356be0eb0d2699144d249e9a899e6ab1716626
MatheusCrivellaro <matheuscriv@gmail.com>
2024-05-26 01:46:25 -0300

    Finalizando conteiners informacionais

--- src/App.js
+++ src/App.js
@@ -22,18 +22,28 @@ function App() {
                         transformam dados em insights valiosos, automatizam processos e aumentam a eficiência operacional
                         das empresas.
                     </InformationText>
-                    <InformationImage image="/images/pexels-packermann-1666021.jpg"/>
+                    <InformationImage image="/images/Logo-novaHorizonte-round-background.png"/>
                 </InformationContent>
 
                 <InformationContent>
-                    <InformationImage image="/images/IMG_2703%20(1).JPG"/>
-                    <InformationText number="01" title="O que é a NovaHorizonte?" preTitle="SOBRE NÓS">
+                    <InformationImage image="/images/pexels-packermann-1666021.jpg"/>
+                    <InformationText number="02" title="Porquê criamos esse site?" preTitle="OBJETIVO">
                         A NovaHorizonte Tecnologia e Inovação é uma empresa fictícia criada para exemplificar um modelo de
                         negócio inovador no setor de tecnologia.
                         Especializada em Inteligência Artificial e Machine Learning, a NovaHorizonte desenvolve soluções que
                         transformam dados em insights valiosos, automatizam processos e aumentam a eficiência operacional
                         das empresas.
                     </InformationText>
+                </InformationContent>
+                <InformationContent>
+                    <InformationText number="03" title="Como posso acessar o sistema?" preTitle="COMO ACESSAR">
+                        A NovaHorizonte Tecnologia e Inovação é uma empresa fictícia criada para exemplificar um modelo de
+                        negócio inovador no setor de tecnologia.
+                        Especializada em Inteligência Artificial e Machine Learning, a NovaHorizonte desenvolve soluções que
+                        transformam dados em insights valiosos, automatizam processos e aumentam a eficiência operacional
+                        das empresas.
+                    </InformationText>
+                    <InformationImage image="/images/pexels-ayan-biswas-109307294-9631566.jpg"/>
                 </InformationContent>
             </div>
         </div>
